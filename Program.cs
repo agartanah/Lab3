3a346b8cad8cf732ab01cf7b02361ce87dc2e3a6
agartanah <vyatin2017@mail.ru>
2023-02-24 14:45:38 +0300

    Create constructor of SquareMatrix and method PrintMatrix

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace Lab3 {
   class Program {
     static void Main(string[] args) {
+      int Side = int.Parse(Console.ReadLine());
+
+      SquareMatrix Array1 = new SquareMatrix(Side);
+      SquareMatrix Array2 = new SquareMatrix(Side);
+
+      Array1.PrintMatrix();
+      Array2.PrintMatrix();
     }
   }
 }
--- SquareMatrix.cs
+++ SquareMatrix.cs
@@ -6,5 +6,25 @@ using System.Threading.Tasks;
 
 namespace Lab3 {
   class SquareMatrix {
+    public int Side;
+    public double[,] ArraySquare;
+    Random ValRandom = new Random(Guid.NewGuid().GetHashCode());
+    public SquareMatrix(int Side) {
+      this.Side = Side;
+      ArraySquare = new double[Side, Side];
+
+      for (int RowIndex = 0; RowIndex < Side; ++RowIndex) {
+        for (int ColumnIndex = 0; ColumnIndex < Side; ++ColumnIndex) {
+          ArraySquare[RowIndex, ColumnIndex] = ValRandom.Next(100);
+        }
+      }
+    }
+    
+    public void PrintMatrix() {
+      foreach (var MatrixElement in ArraySquare) {
+        Console.Write(MatrixElement + " ");
+      }
+      Console.WriteLine();
+    }
   }
 }
