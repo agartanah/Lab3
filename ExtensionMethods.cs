ee76a690fa9139e9ad271558f4472441d6e7d8fe
agartanah <vyatin2017@mail.ru>
2023-05-21 15:58:30 +0300

    Removed unnecessary libraries

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lab3 {
+﻿namespace Lab3 {
   public static class ExtensionMethods {
     public static void Transpose(ref SquareMatrix Matrix) {
       for (int RowIndex = 0; RowIndex < Matrix.Side; ++RowIndex) {
